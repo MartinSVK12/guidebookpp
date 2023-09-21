663c8024dd9f0c8cd02deca687f3459a9709080f
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2023-09-21 13:29:55 +0200

    Fix bug where items disappear when opening the guidebook from a crafting table.

--- src/main/java/sunsetsatellite/guidebookpp/mixin/GuiContainerMixin.java
+++ src/main/java/sunsetsatellite/guidebookpp/mixin/GuiContainerMixin.java
@@ -1,7 +1,10 @@
 package sunsetsatellite.guidebookpp.mixin;
 
 import net.minecraft.client.gui.GuiContainer;
+import net.minecraft.client.gui.GuiCrafting;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.core.player.inventory.Container;
+import net.minecraft.core.player.inventory.ContainerWorkbench;
 import net.minecraft.core.player.inventory.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,6 +22,10 @@ public abstract class GuiContainerMixin extends GuiScreen {
 
     @Shadow protected abstract Slot getSlotAtPosition(int i, int j);
 
+    @Shadow public Container inventorySlots;
+
+    @Shadow public abstract void onGuiClosed();
+
     @Inject(
             method = "keyTyped",
             at = @At("TAIL")
@@ -27,10 +34,14 @@ public abstract class GuiContainerMixin extends GuiScreen {
 
         Slot slot = GuidebookPlusPlus.lastSlotHovered;
         if(slot != null && ((IKeybinds)this.mc.gameSettings).getKeyViewRecipe().isEventKey()){
+            this.mc.thePlayer.closeScreen();
+            onGuiClosed();
             GuidebookPlusPlus.focus = slot.getStack();
             GuidebookPlusPlus.isUsage = false;
             GuidebookPlusPlus.mc.thePlayer.displayGUIGuidebook();
         } else if(slot != null && ((IKeybinds)this.mc.gameSettings).getKeyViewUsage().isEventKey()){
+            this.mc.thePlayer.closeScreen();
+            onGuiClosed();
             GuidebookPlusPlus.focus = slot.getStack();
             GuidebookPlusPlus.isUsage = true;
             GuidebookPlusPlus.mc.thePlayer.displayGUIGuidebook();
